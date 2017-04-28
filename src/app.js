d0898a5fd018c238277adc0eb552b34423c33299
Michael Tolly <miketolly@gmail.com>
2017-04-28 15:31:48 -0500

    Setting up results + species screens

--- src/app.js
+++ src/app.js
@@ -6,50 +6,58 @@ import {
   Text,
   View,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  Navigator
 } from 'react-native';
 import { Map, Set } from 'immutable';
 import update from 'immutability-helper';
 
-import { readCSV, Dataset } from './types';
+import { readCSV, Dataset, Species } from './types';
 
 const datasetCSV: string = "name,description,color,size\ndog,it's a dog,brown,big\ncat,it's a cat,\"white,brown\",small";
 const dataset = new Dataset( readCSV(datasetCSV) );
 
-type NomenState = {
-  selected: Map<string, Set<string>>;
+type AttributesProps = {
+  selected: Map<string, Set<string>>,
+  updateSelected: (Map<string, Set<string>>) => void,
+  goToResults: () => void,
 };
 
-export class NomenNative extends Component<void, {}, NomenState> {
-  state: NomenState;
-
-  constructor(props: {}) {
+class AttributesScreen extends Component<AttributesProps, AttributesProps, void> {
+  constructor(props: AttributesProps) {
     super(props);
-    this.state = { selected: Map() };
   }
 
+  static defaultProps = {
+    selected: Map(),
+    updateSelected: () => {},
+    goToResults: () => {},
+  };
+
   press(k: string, v: string): void {
-    this.setState((state) => update(state, {
-      selected: {
-        $apply: (sel) => sel.update(k, Set(), (vs: Set<string>) => vs.has(v) ? vs.delete(v) : vs.add(v))
-      }
-    }));
+    this.props.updateSelected(
+      this.props.selected.update(
+        k,
+        Set(),
+        (vs: Set<string>) => vs.has(v) ? vs.delete(v) : vs.add(v)
+      )
+    );
   }
 
   isSelected(k: string, v: string): boolean {
-    return this.state.selected.get(k, Set()).has(v);
+    return this.props.selected.get(k, Set()).has(v);
   }
 
   anySelection(): boolean {
-    return this.state.selected.some((set) => set.size !== 0);
+    return this.props.selected.some((set) => set.size !== 0);
   }
 
   clearSearch(): void {
-    this.setState({selected: Map()});
+    this.props.updateSelected(Map());
   }
 
   render() {
-    const scored = dataset.score(this.state.selected);
+    const scored = dataset.score(this.props.selected);
     let perfect = 0;
     for (let [species, score] of scored) {
       if (score == 1) {
@@ -88,9 +96,106 @@ export class NomenNative extends Component<void, {}, NomenState> {
             </TouchableOpacity>
           : undefined
         }
-        <Text style={styles.attrHeader}>{perfect} results</Text>
+        <TouchableOpacity onPress={this.props.goToResults}>
+          <Text style={styles.attrHeader}>{perfect} results</Text>
+        </TouchableOpacity>
       </View>
     );
+  }
+}
+
+type ResultsProps = {
+  results: Array<[Species, number]>,
+  goToAttributes: () => void,
+  goToSpecies: (Species) => void,
+};
+
+class ResultsScreen extends Component<ResultsProps, ResultsProps, void> {
+  static defaultProps = {
+    results: [],
+    goToAttributes: () => {},
+    goToSpecies: () => {},
+  };
+
+  render() {
+    return <View style={styles.outerView}>
+      <TouchableOpacity onPress={this.props.goToAttributes}>
+        <Text>Back to attributes</Text>
+      </TouchableOpacity>
+      {
+        this.props.results.map(([species, score]) =>
+          <TouchableOpacity key={species.name} onPress={() => this.props.goToSpecies(species)}>
+            <Text>{species.name}</Text>
+            <Text>Score: {score}</Text>
+          </TouchableOpacity>
+        )
+      }
+    </View>;
+  }
+}
+
+type SpeciesProps = {
+  species: Species,
+  goToResults: () => void,
+};
+
+type SpeciesPropsDef = {
+  goToResults: () => void,
+};
+
+class SpeciesScreen extends Component<SpeciesPropsDef, SpeciesProps, void> {
+  static defaultProps = {
+    goToResults: () => {},
+  };
+
+  render() {
+    return <View style={styles.outerView}>
+      <TouchableOpacity onPress={this.props.goToResults}>
+        <Text>Back to results</Text>
+      </TouchableOpacity>
+      <Text>Name: {this.props.species.name}</Text>
+      <Text>Description: {this.props.species.description}</Text>
+      <Text>Attributes: {JSON.stringify(this.props.species.attributes)}</Text>
+    </View>;
+  }
+}
+
+type NomenState = {
+  selected: Map<string, Set<string>>,
+  screen:
+    {tag: 'attributes'} |
+    {tag: 'results'} |
+    {tag: 'species', species: Species},
+};
+
+export class NomenNative extends Component<void, {}, NomenState> {
+  state: NomenState;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = { selected: Map(), screen: {tag: 'attributes'} };
+  }
+
+  render() {
+    switch (this.state.screen.tag) {
+      case 'attributes':
+        return <AttributesScreen
+          selected={this.state.selected}
+          updateSelected={(sel) => this.setState({selected: sel})}
+          goToResults={() => this.setState({screen: {tag: 'results'}})}
+        />;
+      case 'results':
+        return <ResultsScreen
+          results={dataset.score(this.state.selected)}
+          goToAttributes={() => this.setState({screen: {tag: 'attributes'}})}
+          goToSpecies={(spec) => this.setState({screen: {tag: 'species', species: spec}})}
+        />;
+      case 'species':
+        return <SpeciesScreen
+          species={this.state.screen.species}
+          goToResults={() => this.setState({screen: {tag: 'results'}})}
+        />;
+    }
   }
 }
 
